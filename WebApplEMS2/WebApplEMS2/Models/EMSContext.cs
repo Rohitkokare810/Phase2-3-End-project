c862d8f95ec082dd699a76c63789a3010500e782
RohitKokare810 <Rohitkokare810@gmail.com>
2024-01-02 17:16:04 +0530

    initial commit

---
+++ WebApplEMS2/WebApplEMS2/Models/EMSContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApplEMS2.Models
+{
+    public partial class EMSContext : DbContext
+    {
+        public EMSContext()
+        {
+        }
+
+        public EMSContext(DbContextOptions<EMSContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("server=DESKTOP-D9AQSGN;database=EMS;trusted_connection=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasKey(e => e.Eid)
+                    .HasName("PK__Employee__C190176B7390B2A5");
+
+                entity.ToTable("Employee");
+
+                entity.Property(e => e.Eid)
+                    .ValueGeneratedNever()
+                    .HasColumnName("EId");
+
+                entity.Property(e => e.Doj)
+                    .HasColumnType("datetime")
+                    .HasColumnName("DOJ");
+
+                entity.Property(e => e.Fname)
+                    .HasMaxLength(50)
+                    .HasColumnName("FName");
+
+                entity.Property(e => e.Lname)
+                    .HasMaxLength(50)
+                    .HasColumnName("LName");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ WebApplEMS2/WebApplEMS2/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplEMS2.Models
+{
+    public partial class Employee
+    {
+        public int Eid { get; set; }
+        public string? Fname { get; set; }
+        public string? Lname { get; set; }
+        public DateTime? Doj { get; set; }
+        public double? Salary { get; set; }
+    }
+}
